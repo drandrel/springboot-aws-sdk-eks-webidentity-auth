3feca5a4905af7b9b60fe46a92da963b1ae97a32
Andre Rocha <114980894+drandrel@users.noreply.github.com>
2024-05-07 17:57:02 +0200

    Update Controller.java

--- src/main/java/com/example/springboot/Controller.java
+++ src/main/java/com/example/springboot/Controller.java
@@ -5,6 +5,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 @RestController
@@ -29,13 +33,27 @@ public class Controller {
                 .body(s3Service.listBuckets());
     }
 
+    @PostMapping("/upload/{useEncryption}")
+    public ResponseEntity<String> uploadFile(@RequestPart(value = "file") MultipartFile file, @PathVariable Boolean useEncryption) {
+        try {
+            if(useEncryption) {
+                s3Service.uploadFileWithEncryption(file);
+            } else {
+                s3Service.uploadFile(file);
+            }
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("{ \"status\": \"ok\" }");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @GetMapping("/view/{fileName}")
     public ResponseEntity<InputStreamResource> viewFile(@PathVariable String fileName) {
-        var s3Object = s3Service.getFile(fileName);
-        var content = s3Object.asInputStream();
+        var content = s3Service.getFile(fileName);
         return ResponseEntity.ok()
-                .contentType(MediaType.IMAGE_PNG) // This content type can change by your file :)
+                .contentType(MediaType.IMAGE_PNG)
                 .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\""+fileName+"\"")
                 .body(new InputStreamResource(content));
     }
