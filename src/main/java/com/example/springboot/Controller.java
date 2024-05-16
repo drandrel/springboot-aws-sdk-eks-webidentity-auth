131688a3fb2b465d3aa8b3def4f9c67eb73c8372
Andre Rocha <114980894+drandrel@users.noreply.github.com>
2024-05-16 18:01:41 +0200

    Update Controller.java

--- src/main/java/com/example/springboot/Controller.java
+++ src/main/java/com/example/springboot/Controller.java
@@ -7,6 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.amazonaws.services.s3.model.Bucket;
+
+import jakarta.websocket.server.PathParam;
+
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
@@ -25,6 +29,14 @@ public class Controller {
     public String health() {
         return "UP";
     }
+    @PostMapping("/createBucket/{bucketName}")
+    public ResponseEntity<Bucket> createBucket(@PathVariable(value = "bucketName") String bucketName) {
+        try {
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(s3Service.createBucket(bucketName));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @GetMapping("/list_buckets")
     public ResponseEntity<List<String>> listBuckets() {
@@ -34,10 +46,30 @@ public class Controller {
     }
 
     @PostMapping("/upload/{useEncryption}")
-    public ResponseEntity<String> uploadFile(@RequestPart(value = "file") MultipartFile file, @PathVariable Boolean useEncryption) {
+    public ResponseEntity<String> uploadFile(@RequestPart(value = "file") MultipartFile file, @PathVariable(value = "useEncryption") Boolean useEncryption) {
         try {
             if(useEncryption) {
                 s3Service.uploadFileWithEncryption(file);
+            } else {
+                s3Service.uploadFile(file);
+            }
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("{ \"status\": \"ok\" }");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostMapping("/upload/{bucket}/{folder}/{useEncryption}")
+    public ResponseEntity<String> uploadFileToBucket(
+            @RequestPart(value = "file") MultipartFile file,
+            @PathVariable(value = "bucket") String bucket,
+            @PathVariable(value = "folder") String folder,
+            @PathVariable(value = "useEncryption") Boolean useEncryption) {
+        try {
+            if(useEncryption) {
+                s3Service.uploadFileWithEncryption(file, bucket, folder);
             } else {
                 s3Service.uploadFile(file);
             }
