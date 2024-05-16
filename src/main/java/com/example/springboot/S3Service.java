9ee930d90f0d7fc9052f29c09130fd732736f887
Andre Rocha <114980894+drandrel@users.noreply.github.com>
2024-05-16 18:01:30 +0200

    Update S3Service.java

--- src/main/java/com/example/springboot/S3Service.java
+++ src/main/java/com/example/springboot/S3Service.java
@@ -1,5 +1,7 @@
 package com.example.springboot;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.*;
 import lombok.extern.log4j.Log4j2;
@@ -12,7 +14,6 @@ import java.util.Date;
 import java.util.List;
 
 @Service
-@Log4j2
 public class S3Service {
 
     private final AmazonS3 s3client;
@@ -24,6 +25,11 @@ public class S3Service {
         this.s3client = s3client;
     }
 
+    public Bucket createBucket(String bucketName) throws SdkClientException, AmazonServiceException {
+        var bucket = this.s3client.createBucket(bucketName);
+        return bucket;
+    }
+
     public S3ObjectInputStream getFile(String key) {
         return s3client.getObject(bucketName, key).getObjectContent();
     }
@@ -33,26 +39,59 @@ public class S3Service {
     }
 
     public void uploadFile(MultipartFile file) throws IOException, NoSuchAlgorithmException {
-        uploadFile(file, s3client, false);
+        uploadFile(file, s3client, this.bucketName, "/", false);
     }
 
     public void uploadFileWithEncryption(MultipartFile file) throws IOException, NoSuchAlgorithmException {
-        uploadFile(file, s3client, true);
+        uploadFile(file, s3client, this.bucketName, "/", true);
     }
 
-    public void uploadFile(MultipartFile file, AmazonS3 s3client, Boolean encrypt) throws IOException, NoSuchAlgorithmException {
+    public void uploadFileWithEncryption(MultipartFile file, String bucketName, String folder) throws IOException, NoSuchAlgorithmException {
+        uploadFile(file, s3client, bucketName, folder, true);
+    }
+
+    public void uploadFile(MultipartFile file, AmazonS3 s3client, String bucketName, String folder, Boolean encrypt) throws IOException, NoSuchAlgorithmException {
         String fileName = new Date().toString().replace(" ", "-") + "-" + file.getOriginalFilename().replace(" ", "_");
         var objMetadata = new ObjectMetadata();
         byte[] objectBytes = file.getBytes();
         objMetadata.setContentLength(objectBytes.length);
 
+        if(!folder.equals("/")) {
+            if(encrypt){
+                final InputStream im = new InputStream() {
+                    @Override
+                    public int read() throws IOException {
+                        return -1;
+                    }
+                };
+                var folderMetadata = new ObjectMetadata();
+                folderMetadata.setContentLength(0L);
+                folderMetadata.setSSEAlgorithm(ObjectMetadata.AES_256_SERVER_SIDE_ENCRYPTION);
+                var putObjectRequest = new PutObjectRequest(bucketName, folder, im, folderMetadata);
+                var putObjectResult = s3client.putObject(putObjectRequest);
+                System.out.println("FOlder created with sse result: " + putObjectResult.getSSEAlgorithm());
+            } else {
+                final InputStream im = new InputStream() {
+                    @Override
+                    public int read() throws IOException {
+                        return -1;
+                    }
+                };
+                var folderMetadata = new ObjectMetadata();
+                folderMetadata.setContentLength(0L);
+                var putObjectRequest = new PutObjectRequest(bucketName, folder, im, objMetadata);
+                var putObjectResult = s3client.putObject(putObjectRequest);
+                System.out.println("FOlder created with sse result: " + putObjectResult.getSSEAlgorithm());
+            }
+        }
+
         if (encrypt) {
             objMetadata.setSSEAlgorithm(ObjectMetadata.AES_256_SERVER_SIDE_ENCRYPTION);
-            var putObjectRequest = new PutObjectRequest(bucketName, fileName, new ByteArrayInputStream(objectBytes), objMetadata);
+            var putObjectRequest = new PutObjectRequest(bucketName, !folder.equals("/") ? folder + "/" + fileName : fileName, new ByteArrayInputStream(objectBytes), objMetadata);
             var putObjectResult = s3client.putObject(putObjectRequest);
             System.out.println("Upload with sse result: " + putObjectResult.getSSEAlgorithm());
         } else {
-            var putObjectRequest = new PutObjectRequest(bucketName, fileName, new ByteArrayInputStream(objectBytes), objMetadata);
+            var putObjectRequest = new PutObjectRequest(bucketName, !folder.equals("/") ? folder + "/" + fileName : fileName, new ByteArrayInputStream(objectBytes), objMetadata);
             var putObjectResult = s3client.putObject(putObjectRequest);
             System.out.println("Upload with sse result: " + putObjectResult.getSSEAlgorithm());
         }
